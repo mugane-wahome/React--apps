c4b2905d57d1ce57138cd3066312bc92f6a38a4d
mugane-wahome <muganewahome@gmail.com>
2024-06-04 18:25:15 +0300

    Initial commit

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,22 +1,8 @@
 import React from 'react';
-
+import './Bogpost.css'
 const blogPosts = [
-  {
-    id: 1,
-    title: 'Post 1 Title',
-    content: 'This is the content of post 1...',
-    author: 'John Doe',
-    date: '2023-03-15',
-    image: 'https://example.com/post1.jpg',
-  },
-  {
-    id: 2,
-    title: 'Post 2 Title',
-    content: 'This is the content of post 2...',
-    author: 'Jane Doe',
-    date: '2023-03-16',
-    image: 'https://example.com/post2.jpg',
-  },
+  { id: 1, title: 'Post 1 Title', content: 'This is the content of post 1...' },
+  { id: 2, title: 'Post 2 Title', content: 'This is the content of post 2...' },
   // Add more posts here
 ];
 
@@ -25,11 +11,7 @@ const BlogList = () => {
     <div className="blog-list">
       {blogPosts.map((post) => (
         <div key={post.id} className="blog-post">
-          <img src={post.image} alt={post.title} />
           <h3>{post.title}</h3>
-          <p>
-            <small>{post.author} | {post.date}</small>
-          </p>
           <p>{post.content.slice(0, 100)}...</p>
           <a href="#">Read More</a>
         </div>
